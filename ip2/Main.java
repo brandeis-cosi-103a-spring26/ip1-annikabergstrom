f70e870f4855ed1ff90cb866198964140c78fe4a
Annika228 <annikabergstrom@brandeis.edu>
2026-01-25 00:05:22 +0000

    Final skeleton of code. Previous commit had ai code that was automatically added

--- ip2/Main.java
+++ ip2/Main.java
@@ -1,3 +1,5 @@
+import java.util.Map;
+
 public class Main {
     
     public static void main(String[] args) {
@@ -5,52 +7,17 @@ public class Main {
         Player player2 = new Player(2);
         Deck decks = new Deck();
 
-        java.util.Random rand = new java.util.Random();
-        Player[] players = new Player[]{player1, player2};
-        int current = rand.nextInt(2);
-
         while (decks.getNumberFrameworkCards() > 0) {
-            Player p = players[current];
-            // Buy phase: play all cryptocurrency cards in hand for money
-            int money = p.playCryptocurrencies();
-            Card purchased = null;
-
-            // Prefer automation cards (Framework > Module > Method)
-            if (money >= 8 && decks.getAutomationCount("Framework") > 0) {
-                purchased = decks.buyAutomationCard("Framework");
-            } else if (money >= 5 && decks.getAutomationCount("Module") > 0) {
-                purchased = decks.buyAutomationCard("Module");
-            } else if (money >= 2 && decks.getAutomationCount("Method") > 0) {
-                purchased = decks.buyAutomationCard("Method");
-            } else {
-                // If no automation affordable, buy cryptocurrency (best value affordable)
-                if (money >= 6 && decks.getCryptocurrencyCount("Dogecoin") > 0) {
-                    purchased = decks.buyCryptocurrencyCard("Dogecoin");
-                } else if (money >= 3 && decks.getCryptocurrencyCount("Etherium") > 0) {
-                    purchased = decks.buyCryptocurrencyCard("Etherium");
-                } else if (decks.getCryptocurrencyCount("Bitcoin") > 0) {
-                    purchased = decks.buyCryptocurrencyCard("Bitcoin");
-                }
+            // Player 1's turn
+            // Buy phase: TODO
+            player1.cleanUp();
+            if (decks.getNumberFrameworkCards() <= 0) {
+                break;
             }
-
-            if (purchased != null) {
-                p.addToDiscard(purchased);
-            }
-
-            // Cleanup phase
-            p.cleanUp();
-
-            if (decks.getNumberFrameworkCards() <= 0) break;
-            current = 1 - current; // alternate player
+            // Player 2's turn
+            // Buy phase: TODO
+            player2.cleanUp();
         }
-
-        // Game end: compute APs and announce winner
-        int ap1 = player1.totalAutomationPoints();
-        int ap2 = player2.totalAutomationPoints();
-        System.out.println("Game over. Player 1 AP: " + ap1 + " Player 2 AP: " + ap2);
-        if (ap1 > ap2) System.out.println("Player 1 wins!");
-        else if (ap2 > ap1) System.out.println("Player 2 wins!");
-        else System.out.println("Tie!");
 
 
         /**
